b1720974cb8e8d4c116c72978bddefd962b28925
brenner.souza <brenner.souza@gmail.com>
2024-12-30 18:34:51 -0300

    03. Criando Domínios - 04 Ajustando o método listarPets

--- src/main/java/br/com/alura/domain/Abrigo.java
+++ src/main/java/br/com/alura/domain/Abrigo.java
@@ -2,6 +2,7 @@ package br.com.alura.domain;
 
 public class Abrigo {
 
+    private Long id;
     private String nome;
     private String telefone;
     private String email;
@@ -13,4 +14,11 @@ public class Abrigo {
         this.email = email;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
 }
