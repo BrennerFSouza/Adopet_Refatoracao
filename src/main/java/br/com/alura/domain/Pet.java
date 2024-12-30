407ec356ff0c6983d8685bbf0376fb8c35faa879
brenner.souza <brenner.souza@gmail.com>
2024-12-30 18:41:29 -0300

    03. Criando Domínios - 06 Ajustando o método listarPets

--- src/main/java/br/com/alura/domain/Pet.java
+++ src/main/java/br/com/alura/domain/Pet.java
@@ -2,6 +2,7 @@ package br.com.alura.domain;
 
 public class Pet {
 
+    Long id;
     String tipo;
     String nome;
     String raca;
@@ -16,5 +17,33 @@ public class Pet {
         this.idade = idade;
         this.cor = cor;
         this.peso = peso;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public Float getPeso() {
+        return peso;
     }
 }
